c5d8dd4a544c3afac575fee16bd2f79b7bedab02
Harrybsg <50788553+Harrybsg@users.noreply.github.com>
2020-02-17 20:15:07 -0500

    Login secction v2

--- src/controlador/Controlador.java
+++ src/controlador/Controlador.java
@@ -3,6 +3,7 @@ package controlador;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
+
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -10,16 +11,13 @@ import java.io.PrintWriter;
 public class Controlador extends javax.servlet.http.HttpServlet {
     protected void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
-        try(PrintWriter out  = response.getWriter()){
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet Controlador</tilte>");
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet Controlador at "+request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
+        String accion = request.getParameter("accion");
+        switch (accion) {
+            case "Principal":
+                request.getRequestDispatcher("Principal.jsp").forward(request,response);
+                break;
+            default:
+                throw new AssertionError();
         }
     }
 
---
+++ web/Principal.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: harry
+  Date: 17/02/2020
+  Time: 20:04
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Bienvenido</title>
+</head>
+<body>
+
+</body>
+</html>
